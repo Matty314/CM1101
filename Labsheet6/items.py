a7675b3d0f6eb8ca1d20d4d5f66463984ce1946e
Matt <mtthwstevens157@gmail.com>
2018-10-17 15:11:50 +0100

    Second game template

---
+++ Labsheet6/items.py
@@ -0,0 +1,72 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?...""",
+    
+    "mass": "0.2" 
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!",
+        
+    "mass": "1.5"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees.",
+        
+    "mass": "0.5"
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits.",
+        
+    "mass": "0.6"
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen.",
+        
+    "mass": "0.3"
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "description": "This student handbook explains everything. Seriously.",
+        
+    "mass": "0.8"
+}
+
+all_items={
+    "ID": item_id,
+    "Laptop": item_laptop,
+    "Money":item_money,
+    "Biscuits": item_biscuits,
+    "Pen": item_pen,
+    "Handbook": item_handbook
+}
